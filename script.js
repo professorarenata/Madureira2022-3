5304754a51521f9d538d84f1c7d6c037e8ac8a3b
Laboratório Informática <42labinfo@senacrjedu.local>
2023-01-16 20:18:40 -0300

    javascript upgrade 2

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-let resultText = document.querySelector('.result'),
-    v1 = document.querySelector('#n1'),
-    v2 = document.querySelector('#n2')
-    arrBtn = document.querySelector('input[type=button]')
+let arrBtn = document.querySelectorAll('input[type=button]')
+arrBtn.forEach(input => {
+    input.addEventListener('click', () => {
+        let resultText = document.querySelector('.result'),
+        v1 = Number(document.querySelector('#t1').value),
+        v2 = Number(document.querySelector('#t2').value);
+        writeResult(resultText, sumAll(v1, v2, input.value))
+        console.log(v1)
+    })
+});
 
-console.log(arrBtn)
-
-let sumAll = (v, m) => {
+let sumAll = (v, v2, m) => {
  switch (m) {
      case "+":
-         return v+v;
+         return v+v2;
          break;
     
     case "-":
-        return v-v;
+        return v-v2;
         break;
 
     case "*": 
-        return v*v; 
+        return v*v2; 
         break;
     case "/":
-        return v/v; 
+        return v/v2; 
         break;
  }
+},
+
+writeResult = (where,result) => {
+    where.innerHTML = String(result)
 }
 
-function soma(form1)
-{
-    a=new Number(form1.t1.value);
-    b=new Number(form1.t2.value);
-    c=new Number(a+b);
-    resultText.innerHTML = String(c)
-}
-  
-function sub(form1)
-{
-    a=new Number(form1.t1.value);
-    b=new Number(form1.t2.value);
-    c=new Number(a-b);
-    resultText.innerHTML = String(c)
-
-}
 
