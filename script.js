45b54b77c0631c0328eee41ad0de23f71229b47f
Laboratório Informática <42labinfo@senacrjedu.local>
2023-01-16 20:20:22 -0300

    js upgrade 03

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-let arrBtn = document.querySelectorAll('input[type=button]')
-arrBtn.forEach(input => {
-    input.addEventListener('click', () => {
-        let resultText = document.querySelector('.result'),
-        v1 = Number(document.querySelector('#t1').value),
-        v2 = Number(document.querySelector('#t2').value);
-        writeResult(resultText, sumAll(v1, v2, input.value))
-        console.log(v1)
-    })
-});
+let arrBtn = document.querySelectorAll('input[type=button]'),
 
-let sumAll = (v, v2, m) => {
+
+sumAll = (v, v2, m) => {
  switch (m) {
      case "+":
          return v+v2;
@@ -32,4 +24,12 @@ writeResult = (where,result) => {
     where.innerHTML = String(result)
 }
 
+arrBtn.forEach(input => {
+    input.addEventListener('click', () => {
+        let resultText = document.querySelector('.result'),
+        v1 = Number(document.querySelector('#t1').value),
+        v2 = Number(document.querySelector('#t2').value);
+        writeResult(resultText, sumAll(v1, v2, input.value))
+    })
+});
 
